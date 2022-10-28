258e0fe07892f014776d97c9afff42ad2a78f377
qblassk <g.bulaschevich@gmail.com>
2022-10-28 12:10:50 -0300

    port config

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const mainRoutes = require('./router/mainRoutes')
+
+const app = express();
+const publicFolderPath = path.resolve('public');
+
+app.use(express.static(publicFolderPath));
+
+APP_PORT = process.env.PORT || 3000;
+app.listen(APP_PORT, () =>
+    console.log('Server running on port'+ ' ' + APP_PORT));
+
